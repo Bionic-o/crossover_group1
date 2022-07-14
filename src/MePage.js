bf1f289cbcf4afe732578dc52b64e5d167f5bb46
Nico <weiskopf.bionico@gmail.com>
2022-07-14 21:16:52 +0200

    add components

---
+++ src/MePage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MePage () {
+    return (
+
+    )
+}
+
+export default MePage
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Main/Header';
+import MePage from './MePage';
+import NewsFeed from './Main/NewsFeed';
+import AboutUser from './AboutUser';
+import Footer from './Main/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MePage />
+    <Header />
+    <NewsFeed />
+    <AboutUser />
+    <Footer />
   </React.StrictMode>
 );
 
