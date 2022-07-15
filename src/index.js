33c4e1f7565245976b212a79215d951fecc5c6d9
Nico <weiskopf.bionico@gmail.com>
2022-07-15 13:30:40 +0200

    navigate from MePage to Main
    add Routing to App.js
    add CSS to MePage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MePage from './MePage';
+import App from './App';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MePage />
+    <App />
   </React.StrictMode>
 );
 
