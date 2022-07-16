27f0a83bb5bd564fb5aaf67d834224ca15b6cb19
a-lavi <ali.rahmani.63@icloud.com>
2022-07-16 12:50:18 +0200

    fetch user

--- src/App.js
+++ src/App.js
@@ -4,37 +4,47 @@ import {  Routes, Route } from "react-router-dom";
 
 import Main from "./Main";
 import MePage from "./MePage";
-const users = [
-  {
-    id: 1,
-    username : "jack",
-    email: "hey@hoo.com",
-    phone: 1231231234
 
-  },{
-    id: 2,
-    username : "jill",
-    email: "hoo@hoo.com",
-    phone: 1231231234
-
-  }
-]
 const App = () => {
+  const testusers = [
+    {
+      id: 1,
+      username : "jack",
+      email: "hey@hoo.com",
+      phone: 1231231234,
+       twitts: ['test', 'test2' ,'test3']
+  
+    },{
+      id: 2,
+      username : "jill",
+      email: "hoo@hoo.com",
+      phone: 1231231234,
+      twitts: ['test', 'test2' ,'test3']
+  
+    }
+  ]
+  const[ users,setUsers]= useState(testusers)
+  const   [randomUser, setRandomUser]= useState({})
+useEffect(()=> { getUsers()
 
-  /* const getUsers = async ()=>{
-    const response = await fetch('https://twitter-clone-crossover.herokuapp.com/users')
-    const result =await response.json()
+},[users])
+  const getUsers = async ()=>{
+    const response = await fetch(`https://twitter-clone-crossover.herokuapp.com/me`)
+    const result = await response.json()
     console.log(result)
+    setUsers(result)
+    /* const randomIndex = Math.floor(Math.random()* result.length ) 
+    setRandomUser(result[randomIndex]) */
   }
-  getUsers() */
-  const   [cloneUser, setCloneUser]= useState(users)
-console.log(cloneUser)
+  
+  
+console.log(randomUser)
   return (
     <div>
       
         <Routes>
-          <Route path="/" element={<MePage users={users} setCloneUser={setCloneUser} cloneUser={cloneUser} />} />
-          <Route path="/main" element={<Main />} />
+          <Route path="/" element={<MePage users={users}  />} />
+          <Route path="/main" element={<Main  users={users}/>} />
         </Routes>
       
     </div>
--- src/MePage.js
+++ src/MePage.js
@@ -2,23 +2,22 @@ import React from "react";
 import {Link, useNavigate} from "react-router-dom";
 import "./MePage.css";
 
-function MePage ({cloneUser}) {  
-    const randomUser = Math.floor(Math.random()* cloneUser.length ) 
-   console.log(cloneUser.length) 
+function MePage ({users}) { 
+    
    const navigate = useNavigate() 
 
     return (
         <div className="MeProfile">
             <nav className="LogIn">
-                <Link className="navBtn" to="/main">Sign In</Link>
+                <Link className="navBtn" to="/main" >Sign In</Link>
                 <button className="navBtn" onClick={() => navigate("/main")}>Password</button>
             </nav>
             <img className="myProfileImg" src="https://p.kindpng.com/picc/s/699-6997496_round-avatar-hd-png-download.png" alt="error display"></img>
             <div className="UserProperties">
-                <p>UserName :{cloneUser[randomUser].username}</p>
-                <p>#id : {cloneUser[randomUser].id}</p>
-                <p>E-Mail: {cloneUser[randomUser].email}</p>
-                <p>Phone : {cloneUser[randomUser].phone}</p>
+                <p>UserName :{users.twitter_name}</p>
+                <p>#id : {users._id}</p>
+                <p>E-Mail: {users.email}</p>
+                <p>Phone : {users.phone}</p>
             </div>
         </div>
     )
